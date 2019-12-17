37dadb1f7ddfc7a861303224c563dc895c0d1402
Styn van de Haterd <styn.haterd@rocdeleijgraaf.nl>
2019-12-17 22:49:53 +0100

    Small rename

--- Systems.h
+++ Systems.h
@@ -11,7 +11,7 @@
 
 FC_Font* debugFont;
 
-void ViewportSystem(Viewport* _viewport, const Position* _followerPosition, uint16_t i, uint16_t i1);
+void ViewportSystem(Viewport* _viewport, const Position* _followerPosition, uint16_t _maxWidth, uint16_t _maxHeight);
 void velocitySystem(Position* _position, const Velocity* _velocity);
 void drawSystem(SDL_Renderer* _renderer, const Viewport* _viewport, const Position* _position, const Rotation* _rotation, const Sprite* _sprite, bool _debuggable);
 void collisionSystem(EntityData* _data, const Collider* _collider, const Position* _position, const Dimension* _dimension, Velocity* _velocity);
